5492a490116195d892ed6f9616347be9e93cb6c1
Carlos Aragon <aragoncarlosfernando@gmail.com>
2022-07-30 12:16:59 -0300

    Agregar archivos de proyecto.

---
+++ POO ejercicio 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace POO_ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const string FijarObjetivo = "O";
+           const string AgregarSesion = "A";
+           const string Estadisticas = "E";
+           const string Salir = "S";
+            decimal KmTotalRecorrido = 0;
+            decimal TotalOjetivoKm = 0;
+
+            string Opcion = "";
+            do
+            {
+                Console.WriteLine("Menú Principal");
+                Console.WriteLine(FijarObjetivo+": Fijar Objetivo ");
+                Console.WriteLine(AgregarSesion+": Agregar Sesión");
+                Console.WriteLine(Estadisticas+": Estadistica");
+                Console.WriteLine(Salir+": Salir");
+                
+                Opcion = ConvalidarDatos.PedirStrNoVac("Ingrese una opción");
+
+                switch (Opcion)
+                {
+                    case FijarObjetivo:
+                        TotalOjetivoKm = ConvalidarDatos.PedirLong("Ingrese el objetivo",1,500);
+                        Runner.Objetivos(TotalOjetivoKm);
+                        break;
+                    case AgregarSesion:
+                        KmTotalRecorrido = ConvalidarDatos.PedirLong("Agrege su Sesión", 1, 50);
+                        Runner.GuardarSesion(KmTotalRecorrido);
+                        break;
+                    case Estadisticas:
+                        Runner.Estadisticas();
+                        break;
+                    case Salir:
+                        break;
+                    default:
+                        Console.WriteLine("La opción no es valida");
+                        break;
+
+
+                }
+            } while (Opcion != Salir);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ POO ejercicio 2/Runner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_ejercicio_2
+{
+    class Runner
+    {
+        private static decimal totalKm = 0;
+        private static decimal kmRecorrido = 0; 
+        private static decimal objetivoKm = 0;
+        public static void  GuardarSesion(decimal recorridokm)
+        {
+            kmRecorrido = recorridokm;
+            totalKm = totalKm + kmRecorrido;
+        }
+        public static void Objetivos(decimal kmOjetivo)
+        {
+            objetivoKm = kmOjetivo;
+        }
+        public static void Estadisticas()
+        {
+           
+           
+            if (totalKm > objetivoKm)
+            {
+                Console.WriteLine("Estadistica: \n" + "\t Objetivo en Kms Totales: " + objetivoKm + "\t "+ "Realizados : " 
+                    + totalKm + "\tObjetivo Cumplido");
+              
+
+            }
+            else
+            {
+                Console.WriteLine("Estadistica \n" + "Objetivo en Kms Totales: " + objetivoKm + "\t ");
+                Console.WriteLine("Realizados : " + totalKm );
+            }
+        }
+
+    }
+}
